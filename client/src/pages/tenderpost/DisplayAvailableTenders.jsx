a51cefc6f45c22689021b5486fecdcf5d4d61d8f
RonexLemon <ronexondimu@gmail.com>
2023-03-04 00:49:43 +0300

    implement tendepost page

---
+++ client/src/pages/tenderpost/DisplayAvailableTenders.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Banner from "../../components/Banner";
+
+
+const DisplayTenders = (props) => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, []);
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="">
+      <div className="w-[94%]  mx-auto my-10">
+      <Banner/>
+        <div>
+          <h1 className=" text-3xl font-extrabold">Listed Tenders</h1>
+
+          <table className="bg-white w-full table-auto my-10">
+            <thead className="border-b border-gray-200 hover:bg-gray-100">
+              <tr className="bg-white text-gray-500 text-sm leading-normal rounded-lg">
+                <th className="py-3 px-6 text-left">Company Name</th>
+                <th className="py-3 px-6 text-left">Email</th>
+                <th className="py-3 px-6 text-left">Phone Number</th>
+                <th className="py-3 px-6 text-left">Tender Description</th>
+                <th className="py-3 px-6 text-left">Deadline Date</th>
+                <th className="py-3 px-6 text-left">Tender Amount</th>
+                <th className="py-3 px-6 text-center">Action</th>
+              </tr>
+            </thead>
+            <tbody className="text-[#130026]  text-sm font-light">
+              {props.tenders.map((tender, index) => (
+                <>
+                  <tr
+                    key={index}
+                    className="border-b border-gray-200  hover:bg-gray-100"
+                  >
+                    <td className="py-3 px-2 text-left whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-medium font-josefin">
+                          {tender.companyName}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-2 text-left ">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-josefin font-normal">
+                          {tender.email}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-2 text-left">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-josefin font-normal">
+                        {tender.contactEmail}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-2 text-left">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-josefin font-normal">
+                          {tender.tenderDescription}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-2 text-left">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-josefin font-normal">
+                          {tender.deadlineDate}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-6 text-left">
+                      <div className="flex items-center">
+                        <div className="mr-2"></div>
+                        <span className="font-josefin font-normal">
+                          {tender.tenderAmount / 1}
+                        </span>
+                      </div>
+                    </td>
+                    <td className="py-3 px-2 text-center">
+                      <div className="flex item-center justify-center">
+                        <button
+                          className="px-6 py-2 font-normal text-white bg-button-color rounded-md "
+                          onClick={() =>
+                            navigate("/BiderForm", { state: { id: index } })
+                          }
+                          id="btn-bid"
+                        >
+                          BID
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+//     const navigate = useNavigate();
+
+//     return tenders.map(tender => (
+
+//         <div className='tenderCard' key ={tender.contact}>
+//             <div className='tenderCardHeader' id='tenderCardHeader'>
+
+//                         <p><RiBuilding2Fill/><b> {tender.companyName}</b></p>
+//                         <p>{tender.description}</p>
+//                         <h4>{tender.amount}</h4>
+
+//             </div>
+//             <div className='tenderCard-middle' id='tendercard-middle'>
+//                 <h5><GiRotaryPhone/>&nbsp;{tender.contact}&emsp;&emsp;&emsp;&emsp;<MdDateRange/>&nbsp;{tender.deadline} &emsp;&emsp;&emsp;&emsp;<HiOutlineMail/>&nbsp;{tender.email}</h5>
+//             </div>
+//             <div className='bid-btn-approve-btn' id='bid-btn-approve-btn'>
+//                 <button className='btn-bid' id='btn-bid' onClick={ () =>navigate("/BiderForm")}>BID</button>
+//                 <button className='btn-aprove'>Approve</button>
+//                 <button className="deletebtn" onClick={()=> deleteTender(tender.contact)}><BsTrash/></button>
+//             </div>
+
+//         </div>
+
+//      ))
+
+//   }
+export default DisplayTenders;
---
+++ client/src/pages/tenderpost/Tenders.jsx
@@ -0,0 +1,308 @@
+import React from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+
+import { BiderAbi } from "../../abi/bidercontract_abi";
+
+import { useNavigate } from "react-router-dom";
+
+
+const Tenders = () => {
+  let ref = useRef(null);
+  let getformdiv = useRef(null);
+  let btnapprove = useRef(null);
+  //let Tenders =[];
+  const [Tenders, setTenders] = useState([]);
+  const TenderOwnerAddress = "0x21ba8e6B05c8020d985777Ab10457cE7C0626fa1";
+  const [tenderslength, setLength] = useState(0);
+  const web3ModalRef = useRef();
+  const [walletconnect, setWalletConnect] = useState(false);
+  const [address, setaddress] = useState(null);
+  const [userAccount, setUserAccount] = useState(null);
+  //const [accountBalance,setAccountBalance] = useState(null);
+  //postTender to the contract
+
+  //  document.querySelector(".btnPost")
+  //  .addEventListener("click", async (e) =>
+  const btnPosts = async () => {
+    const params = [companyName, description, deadline,contact, email, amount];
+
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tenderContract = new Contract(TenderOwnerAddress, BiderAbi, signer);
+      const results = await tenderContract.writeTenderDetails(...params);
+      // .send({from: address})
+      alert("add results successful");
+      alert(`🎉 You successfully added "${params[0]}".`);
+    } catch (error) {
+      alert("the error is", error);
+      console.log(error);
+    }
+    
+    // getAllTenders()
+  };
+
+  const getUserAddress = async () => {
+    const signer = await getProviderOrSigner(true);
+    const accounts = await signer.getAddress();
+    setaddress(accounts);
+  };
+  const navigate = useNavigate();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    //connect metamask
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    //check if user is connected to Mumbai network
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 3141) {
+      window.alert("Change network to HyperSpace fileCoin");
+      throw new Error("Change network To HyperSpace fileCoin ");
+    }
+    // alert("network is Mumbai")
+    //if need signer for transactions
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      const accounts = await signer.getAddress();
+      setaddress(accounts);
+      return signer;
+    }
+    return web3Provider;
+  };
+  const DectectWindow = async () => {
+    try {
+      await getProviderOrSigner();
+      await getUserAddress();
+      alert("set account");
+      setWalletConnect(true);
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  };
+
+  //main array of tender details objects state
+  //const[tenders, setTenders] = useState(getDatafromLS());
+
+  //input field states
+  const [companyName, setCompanyName] = useState("");
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [contact, setContact] = useState("");
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState("");
+
+  // //Form submit event
+  const handleAddTender = (e) => {
+    //prevent page refresh
+    e.preventDefault();
+
+    // //creating an object
+    // let tender={
+    //     companyName,
+    //     description,
+    //     deadline,
+    //     contact,
+    //     email,
+    //     amount
+    // }
+    // setTenders([...tenders, tender]);
+    setCompanyName("");
+    setDescription("");
+    setDeadline("");
+    setContact("");
+    setEmail("");
+    setAmount("");
+  };
+  
+
+  const openForm = () => {
+    getformdiv.current.style.display = "block";
+  };
+  const closeForm = () => {
+    getformdiv.current.style.display = "none";
+  };
+
+  //delete tender from local storage
+  // const deleteTender=(contact)=>{
+  //     const filteredTenders=tenders.filter((element, index)=>{
+  //         return element.contact !== contact
+  //     })
+  //     setTenders(filteredTenders);
+  // }
+  //saving data to local storage
+
+  // useEffect(()=>{
+  //     localStorage.setItem('tenders', JSON.stringify(tenders));
+  // }, [tenders])
+  //load content on reload
+  useEffect(() => {
+    web3ModalRef.current = new Web3Modal({
+      network: "hyperspace",
+      providerOptions: {},
+      disableInjectedProvider: false,
+      cacheProvider: false,
+    });
+    //getTotalTendersLength();
+    // getAllTenders();
+    getProviderOrSigner();
+    //renderProducts();
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [walletconnect, tenderslength]);
+
+  return (
+    <>
+      <div className="flex">
+        
+        <div className="mx-auto w-[95%] my-10">
+          <div ref={getformdiv} className="">
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <h1 className="font-jakarta text-3xl font-extrabold">
+                  Advertise Tender
+                </h1>
+                <p className="py-4 pr-4 font-josefin">
+                  Connect wallet to fill the form below.
+                </p>
+              </div>
+
+              {/* <div>
+                <button
+                  onClick={DectectWindow}
+                  className="px-4 py-2 font-josefin text-white bg-primary-color rounded-full shadow-md hover:shadow-lg"
+                >
+                  Connect Wallet
+                </button>
+              </div> */}
+            </div>
+
+            <div className="bg-white w-7/12 shadow-sm my-4 p-10 rounded-md">
+              <div className="my-6">
+                <form
+                  onSubmit={handleAddTender}
+                  className="flex space-x-24"
+                >
+                  <div className="space-y-4">
+                    <div className="w-full">
+                      <label className="font-josefin">Company Name</label>
+                      <br />
+                      <input
+                        className="py-3 pr-24 pl-4 border-2 rounded-lg"
+                        type="text"
+                        id="company"
+                        name="company"
+                        placeholder="Company Name..."
+                        required
+                        onChange={(e) => setCompanyName(e.target.value)}
+                        value={companyName}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="font-josefin pt-2">
+                        Tender Description
+                      </label>
+                      <br />
+                      <input
+                        className="py-3 pr-24 pl-4 border-2 rounded-lg"
+                        type="text"
+                        id="description"
+                        name="description"
+                        placeholder="Tender description..."
+                        required
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="font-josefin">DeadLine</label>
+                      <br />
+                      <input
+                        className="py-3 px-4 border-2 rounded-lg"
+                        type="date"
+                        id="deadline"
+                        name="deadline"
+                        required
+                        onChange={(e) => setDeadline(e.target.value)}
+                        value={deadline}
+                      />
+                    </div>
+                  </div>
+                  <div className="space-y-4">
+                    <div>
+                      <label className="font-josefin">Contact</label>
+                      <br />
+                      <input
+                        className="py-3 pr-24 pl-4 border-2 rounded-lg"
+                        type="text"
+                        id="contact"
+                        name="contact"
+                        placeholder="0792271915"
+                        required
+                        onChange={(e) => setContact(e.target.value)}
+                        value={contact}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="font-josefin">Email</label>
+                      <br />
+                      <input
+                        className="py-3 pr-24 pl-4 border-2 rounded-lg"
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="stansmith@gmail.com"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="font-josefin">Amount</label>
+                      <br />
+                      <input
+                        className="py-3 pr-24 pl-4 border-2 rounded-lg"
+                        type="text"
+                        id="amount"
+                        name="amount"
+                        placeholder="1,300,000"
+                        required
+                        onChange={(e) => setAmount(e.target.value)}
+                        value={amount}
+                      />
+                    </div>
+
+                    <div className="flex justify-between my-4">
+                      <button
+                        className="px-10 py-2 border-2 border-secondary-color text-gray-300 rounded-md mb-2 font-josefin"
+                        onClick={() => navigate("/Tenders")}
+                      >
+                        Close
+                      </button>
+                      <button
+                        className="px-10 py-2 bg-button-color text-[#fff] rounded-md shadow-md mb-2 font-josefin"
+                        onClick={() => {
+                          btnPosts();
+                        }}
+                        type="submit"
+                        value="Submit"
+                      >
+                        Post Tender
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Tenders;
