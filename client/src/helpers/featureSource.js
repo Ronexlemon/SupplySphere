1b90bde70403efd6e63c5825dc6850eacf55d0fc
RonexLemon <ronexondimu@gmail.com>
2023-03-04 00:38:06 +0300

    add helpers

---
+++ client/src/helpers/featureSource.js
@@ -0,0 +1,27 @@
+export const featureSource = [
+    {
+      id: 0,
+      title: "Browse the list of tenders",
+      subtitle:
+        "Best way to collect review. Invite your client and get review from client.",
+    },
+    {
+      id: 1,
+      title: "Advertise Tenders",
+      subtitle:
+        "Find the right freelancer to begin working on your project within minutes.",
+    },
+    {
+      id: 2,
+      title: "Track Tender applications",
+      subtitle:
+        "Find out project which match your skill and grab the opprtunity to work with popular companies.",
+    },
+    {
+      id: 1,
+      title: "View tender allocations",
+      subtitle:
+        "Best way to show your outstanding skill by creating portflio and get more clients.",
+    },
+  ];
+  
