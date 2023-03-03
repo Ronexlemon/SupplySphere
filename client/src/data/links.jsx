724b37e31ceb7fbce61d1ad001f821b491b7e6b2
RonexLemon <ronexondimu@gmail.com>
2023-03-04 00:36:15 +0300

    add links

---
+++ client/src/data/links.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+export const links = [
+  {
+    title: "Tenders",
+    links: [
+      {
+        name: "Available Tenders",
+      },
+      {
+        name: "Post Tenders",
+      },
+      {
+        name: "Tender Allocation",
+      },
+      {
+        name: "Tender Status",
+      },
+      {
+        name: "Approve",
+      },
+    ],
+  },
+  {
+    title: "User",
+    links: [
+      {
+        name: "My bids",
+      },
+      {
+        name: "My tenders",
+      },
+      
+    ],
+  },
+];
