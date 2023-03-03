a86420b3cb8bd4211328611c80ca29b62ca26676
RonexLemon <ronexondimu@gmail.com>
2023-03-04 00:52:20 +0300

    landing page

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection';
+
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
+
+export default LandingPage
